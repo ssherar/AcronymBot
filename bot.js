f1bb9d30cf2a568dd01b9ee300bbc5883f5f7076
ssherar <s.sherar@live.com>
2013-12-04 22:03:33 +0000

    Bot now responds with the acronym meaning

--- bot.js
+++ bot.js
@@ -1,12 +1,41 @@
 var Twit = require("twit"),
     config = require("./config"),
+    acronym = require("./acronym"),
     apiConfig = config.api,
     botConfig = config.bot;
 
-var T = new Twit(apiConfig);
+// [todo] - Implement a better logging module
+var l = function(level, message) {
+    console.log("[%s - %s] - %s",
+                    new Date(),
+                    level,
+                    message
+    );
+}
 
+l("info", "started script");
+var T = new Twit(apiConfig),
+    acronymMatch = /what is (\w+)\?/g
+l("info", "authenticated against twitter, ready to start listening");
 var stream = T.stream("statuses/filter", {track: botConfig.handle});
 
 stream.on("tweet", function(tweet) {
-    console.log(tweet)
+    l("info", "received tweet: "+tweet.text);
+    var text = tweet.text,
+        user = tweet.user.screen_name;
+    var match = acronymMatch.exec(text);
+    if(match != null) {
+        acronym.findOne({ acronym: match[1]}, function(err, found) {
+            // [todo] - string formatting properly
+            l("debug", "Found from database: " + found.acronym)
+            reply = "@"+user+" "+found.acronym+" = "+ found.meaning;
+            T.post('statuses/update', { status: reply }, function(err, res) {
+                l("info","Reply: "+reply);
+                l("info","Error: "+err);
+                l("info","Resp : "+res);
+            }); 
+        });
+    }
 });
+
+
